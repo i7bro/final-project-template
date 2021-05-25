8aeafe5da73b30abe66456704d8180b63d63f013
Semibratov Ilya <i7brothers@gmail.com>
2021-05-25 14:02:11 +0400

    add fragments page

--- src/main/java/com/epam/rd/izh/RDIzhFinalProjectTemplate.java
+++ src/main/java/com/epam/rd/izh/RDIzhFinalProjectTemplate.java
@@ -24,8 +24,7 @@ import java.util.List;
  */
 @SpringBootApplication
 public class RDIzhFinalProjectTemplate {
-
-
+	
 	public static void main(String[] args) { {}
 		SpringApplication.run(RDIzhFinalProjectTemplate.class, args);
 	}
